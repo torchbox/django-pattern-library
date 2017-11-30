5b65555edf633039acadfc0608f4dd5c2911bab2
Ben Dickinson <ben.dickinson@torchbox.com>
2017-11-30 11:23:13 +0000

    Fix module-level import of Django in pattern_library
    This was causing pip install to fail if Django was not already installed

--- pattern_library/__init__.py
+++ pattern_library/__init__.py
@@ -1,22 +1,39 @@
-from django.conf import settings
+import importlib
+from functools import wraps
+
 
 __version__ = '0.1.1'
 
 default_app_config = 'pattern_library.apps.PatternLibraryAppConfig'
+settings = None
 
 
+def uses_settings(func):
+    @wraps(func)
+    def wrapped():
+        global settings
+        if settings is None:
+            settings = importlib.import_module('django.conf').settings
+        return func()
+    return wrapped
+
+
+@uses_settings
 def get_pattern_template_dir():
     return settings.PATTERN_LIBRARY_TEMPLATE_DIR
 
 
+@uses_settings
 def get_pattern_template_prefix():
     return getattr(settings, 'PATTERN_LIBRARY_TEMPLATE_PREFIX', 'patterns')
 
 
+@uses_settings
 def get_pattern_template_suffix():
     return getattr(settings, 'PATTERN_LIBRARY_TEMPLATE_SUFFIX', '.html')
 
 
+@uses_settings
 def get_pattern_base_template_name():
     return getattr(settings, 'PATTERN_LIBRARY_BASE_TEMPLATE_NAME', 'patterns/base.html')
 
