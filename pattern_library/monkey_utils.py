7e3d7093d83061b5cfbdf161e70e5c0a138ef38d
Mikalai Radchuk <radchuk.m@gmail.com>
2017-12-01 17:21:22 +0000

    Add support of SimpleNode and logging
    Now override_tag supports target_var in SimpleNode
    and helps you to find tags without stub data.

--- pattern_library/monkey_utils.py
+++ pattern_library/monkey_utils.py
@@ -1,4 +1,9 @@
+import logging
+from django.template.library import SimpleNode
+
 from pattern_library.utils import is_pattern_library_context, get_pattern_config, render_pattern
+
+logger = logging.getLogger(__name__)
 
 
 def override_tag(register, name):
@@ -17,8 +22,11 @@ def override_tag(register, name):
 
         def node_render(context):
             if is_pattern_library_context(context):
+                result = ''
+
                 # Load pattern's config
-                pattern_config = get_pattern_config(parser.origin.template_name)
+                current_template_name = parser.origin.template_name
+                pattern_config = get_pattern_config(current_template_name)
 
                 # Extract values for lookup from the token
                 bits = token.split_contents()
@@ -32,15 +40,26 @@ def override_tag(register, name):
                     tag_config = tag_config.get(arguments, {})
 
                     # Render a raw string, if defined
-                    raw_string = tag_config.get('raw')
-                    if raw_string:
-                        return raw_string
+                    result = tag_config.get('raw')
 
-                    # Render pattern if defined
-                    template_name = tag_config.get('template_name')
-                    if template_name:
-                        request = context.get('request')
-                        return render_pattern(request, template_name)
+                    # Render pattern, if raw string is not defined
+                    if not result:
+                        template_name = tag_config.get('template_name')
+                        if template_name:
+                            request = context.get('request')
+                            result = render_pattern(request, template_name)
+
+                if result:
+                    if isinstance(original_node, SimpleNode) and original_node.target_var:
+                        context[original_node.target_var] = result
+                        return ''
+                    else:
+                        return result
+                else:
+                    logger.warning(
+                        'No stub data defined for the "%s" tag in the "%s" template',
+                        tag_name, current_template_name
+                    )
 
             return original_node_render(context)
 
