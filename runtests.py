9321a1c84042676e7479ce824dc42c9b7b87447c
Ben Dickinson <ben.dickinson@torchbox.com>
2018-01-11 09:59:05 +0000

    Add coverage

--- runtests.py
+++ runtests.py
@@ -2,14 +2,22 @@
 import os
 import sys
 
+import coverage
+
 import django
 from django.conf import settings
 from django.test.utils import get_runner
 
+
 if __name__ == "__main__":
+    cov = coverage.Coverage()
+    cov.start()
     os.environ['DJANGO_SETTINGS_MODULE'] = 'tests.test_settings'
     django.setup()
     TestRunner = get_runner(settings)
     test_runner = TestRunner()
     failures = test_runner.run_tests(["tests"])
+    cov.stop()
+    cov.save()
+    cov.html_report()
     sys.exit(bool(failures))
