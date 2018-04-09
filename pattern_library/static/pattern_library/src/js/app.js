c82a2abb769b96bfeb61287ed2961ddd32b9ae5c
Chris Lawton <chris.lawton@torchbox.com>
2018-04-09 16:59:57 +0100

    add basic iframe resizing

--- pattern_library/static/pattern_library/src/js/app.js
+++ pattern_library/static/pattern_library/src/js/app.js
@@ -2,25 +2,34 @@ import '../css/main.scss';
 import hljs from 'highlight.js';
 
 {
-    document.addEventListener('DOMContentLoaded', () => hljs.initHighlightingOnLoad());
+    function highlightSyntax(){
+        hljs.initHighlightingOnLoad();
+    }
 
-        function highlightSyntax(){
-            hljs.initHighlightingOnLoad();
-        }
-
-        function togglePatterns(){
-            const headings = document.querySelectorAll('.js-toggle-pattern');
-
-            headings.forEach(function(heading){
-                heading.addEventListener('click', function(e){
-                    event.target.classList.toggle('is-open');
-                    event.target.nextElementSibling.classList.toggle('is-open');
-                })
+    function togglePatterns(){
+        const headings = document.querySelectorAll('.js-toggle-pattern');
+        headings.forEach(heading => {
+            heading.addEventListener('click', e => {
+                event.target.classList.toggle('is-open');
+                event.target.nextElementSibling.classList.toggle('is-open');
             })
-        }
+        })
+    }
+
+    function toggleIframe(){
+        const buttons = document.querySelectorAll('.js-resize-iframe');
+        buttons.forEach(button => {
+            button.addEventListener('click', (e) => {
+                buttons.forEach(button => button.classList.remove('is-active'));
+                e.target.classList.add('is-active');
+                document.querySelector('.js-iframe').style.width = e.target.dataset.resize == 100 ? `${e.target.dataset.resize}%` : `${e.target.dataset.resize}px`;
+            })
+        });
+    }
 
     document.addEventListener('DOMContentLoaded', () => {
         highlightSyntax();
         togglePatterns();
+        toggleIframe();
     });
 }
