607e99bf7e47b1583844d18bca0b643e4178de39
Chris Lawton <chris.lawton@torchbox.com>
2018-04-09 18:47:39 +0100

    swap babel preset 2015 with babel preset env

--- pattern_library/static/pattern_library/webpack.config.js
+++ pattern_library/static/pattern_library/webpack.config.js
@@ -13,7 +13,7 @@ module.exports = {
                 test: /\.js$/,
                 use: {
                     loader: 'babel-loader',
-                    options: { presets: ['es2015'] }
+                    options: { presets: ['env'] }
                 }
             },
             {
