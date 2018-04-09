bcbcc5b01484b8bd6e0d1e9b9c5ab44af10391d4
Chris Lawton <chris.lawton@torchbox.com>
2018-04-09 15:14:07 +0100

    toggle patterns on click

--- pattern_library/static/pattern_library/src/js/app.js
+++ pattern_library/static/pattern_library/src/js/app.js
@@ -3,4 +3,24 @@ import hljs from 'highlight.js';
 
 {
     document.addEventListener('DOMContentLoaded', () => hljs.initHighlightingOnLoad());
+
+        function highlightSyntax(){
+            hljs.initHighlightingOnLoad();
+        }
+
+        function togglePatterns(){
+            const headings = document.querySelectorAll('.js-toggle-pattern');
+
+            headings.forEach(function(heading){
+                heading.addEventListener('click', function(e){
+                    event.target.classList.toggle('is-open');
+                    event.target.nextElementSibling.classList.toggle('is-open');
+                })
+            })
+        }
+
+    document.addEventListener('DOMContentLoaded', () => {
+        highlightSyntax();
+        togglePatterns();
+    });
 }
