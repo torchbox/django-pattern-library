1bbc59a612b24fdee2b4cafab33ff7e1bc1bf0f2
Chris Lawton <chris.lawton@torchbox.com>
2018-04-09 11:37:00 +0100

    add basic webpack config and import highlight js via npm

---
+++ pattern_library/static/pattern_library/src/js/app.js
@@ -0,0 +1,6 @@
+import '../css/main.scss';
+import hljs from 'highlight.js';
+
+{
+    document.addEventListener('DOMContentLoaded', () => hljs.initHighlightingOnLoad());
+}
---
+++ pattern_library/static/pattern_library/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+
+module.exports = {
+  entry: "./src/js/app.js",
+  output: {
+    path: path.resolve(__dirname, "dist"),
+    filename: "bundle.js",
+    publicPath: "./dist"
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: {
+          loader: "babel-loader",
+          options: { presets: ["es2015"] }
+        }
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          {
+            loader: "style-loader" // creates style nodes from JS strings
+          },
+          {
+            loader: "css-loader" // translates CSS into CommonJS
+          },
+          {
+            loader: "sass-loader" // compiles Sass to CSS
+          }
+        ]
+      }
+    ]
+  }
+};
