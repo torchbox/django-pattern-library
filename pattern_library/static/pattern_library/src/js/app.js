25d7c02eb1e2343e1d2cbe99a4cab2546a8ec2b7
Chris Lawton <chris.lawton@torchbox.com>
2018-04-10 09:30:41 +0100

    lint

--- pattern_library/static/pattern_library/src/js/app.js
+++ pattern_library/static/pattern_library/src/js/app.js
@@ -2,28 +2,31 @@ import '../css/main.scss';
 import hljs from 'highlight.js';
 
 {
-    function highlightSyntax(){
+    function highlightSyntax() {
         hljs.initHighlightingOnLoad();
     }
 
-    function togglePatterns(){
+    function togglePatterns() {
         const headings = document.querySelectorAll('.js-toggle-pattern');
         headings.forEach(heading => {
             heading.addEventListener('click', e => {
                 event.target.classList.toggle('is-open');
                 event.target.nextElementSibling.classList.toggle('is-open');
-            })
-        })
+            });
+        });
     }
 
-    function toggleIframe(){
+    function toggleIframe() {
         const buttons = document.querySelectorAll('.js-resize-iframe');
         buttons.forEach(button => {
-            button.addEventListener('click', (e) => {
+            button.addEventListener('click', e => {
                 buttons.forEach(button => button.classList.remove('is-active'));
                 e.target.classList.add('is-active');
-                document.querySelector('.js-iframe').style.width = e.target.dataset.resize == 100 ? `${e.target.dataset.resize}%` : `${e.target.dataset.resize}px`;
-            })
+                document.querySelector('.js-iframe').style.width =
+                    e.target.dataset.resize == 100
+                        ? `${e.target.dataset.resize}%`
+                        : `${e.target.dataset.resize}px`;
+            });
         });
     }
 
