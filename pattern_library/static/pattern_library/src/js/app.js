964e7c79ea560b912f1f0a431b958d5cb5a52993
Chris Lawton <chris.lawton@torchbox.com>
2018-04-10 10:10:54 +0100

    close the iframe via escape key

--- pattern_library/static/pattern_library/src/js/app.js
+++ pattern_library/static/pattern_library/src/js/app.js
@@ -17,15 +17,30 @@ import hljs from 'highlight.js';
     }
 
     function toggleIframe() {
+        const patternIframe = document.querySelector('.js-iframe');
         const buttons = document.querySelectorAll('.js-resize-iframe');
+
+        // Pop-out iframe
+        document.querySelector('.js-resize-iframe-full').addEventListener('click', () => {
+            patternIframe.style.removeProperty('width');
+            patternIframe.classList.add('is-fullscreen');
+        });
+
+        // Close iframe
+        document.addEventListener('keydown', e => {
+            e = e || window.event;
+            if (e.key === 'Escape') {
+                document.querySelector('.js-iframe').classList.remove('is-fullscreen');
+            }
+        });
+
+        // Resize iframe
         buttons.forEach(button => {
             button.addEventListener('click', e => {
                 buttons.forEach(button => button.classList.remove('is-active'));
                 e.target.classList.add('is-active');
-                document.querySelector('.js-iframe').style.width =
-                    e.target.dataset.resize == 100
-                        ? `${e.target.dataset.resize}%`
-                        : `${e.target.dataset.resize}px`;
+                patternIframe.style.width =
+                    e.target.dataset.resize == 100 ? `${e.target.dataset.resize}%` : `${e.target.dataset.resize}px`;
             });
         });
     }
