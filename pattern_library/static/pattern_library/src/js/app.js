bc434e364cd34067f7311bc219d2d651531da7c6
Chris Lawton <chris.lawton@torchbox.com>
2018-04-12 14:36:33 +0100

    tidy up js

--- pattern_library/static/pattern_library/src/js/app.js
+++ pattern_library/static/pattern_library/src/js/app.js
@@ -14,8 +14,8 @@ import hljs from 'highlight.js/lib/highlight';
         const headings = document.querySelectorAll('.js-toggle-pattern');
         headings.forEach(heading => {
             heading.addEventListener('click', e => {
-                event.target.classList.toggle('is-open');
-                event.target.nextElementSibling.classList.toggle('is-open');
+                e.target.classList.toggle('is-open');
+                e.target.nextElementSibling.classList.toggle('is-open');
             });
         });
     }
@@ -75,7 +75,7 @@ import hljs from 'highlight.js/lib/highlight';
     }
 
     function toggleNav() {
-        document.querySelector('.js-close-menu').addEventListener('click', (e) => {
+        document.querySelector('.js-close-menu').addEventListener('click', e => {
             document.querySelector('body').classList.toggle('nav-closed');
         });
     }
