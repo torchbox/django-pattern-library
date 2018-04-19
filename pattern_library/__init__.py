92b1f1b6d428d1f45e6ca7ce2faffd46141d770d
Mikalai Radchuk <radchuk.m@gmail.com>
2018-04-19 10:50:25 +0100

    Bump version

--- pattern_library/__init__.py
+++ pattern_library/__init__.py
@@ -2,7 +2,7 @@ import importlib
 from functools import wraps
 
 
-__version__ = '0.1.7'
+__version__ = '0.1.8'
 
 default_app_config = 'pattern_library.apps.PatternLibraryAppConfig'
 settings = None
