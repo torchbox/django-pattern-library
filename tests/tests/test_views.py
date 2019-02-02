2440dc3507f9b14fc452d84d772596badf266af8
Todd Dembrey <todd.dembrey@torchbox.com>
2019-02-02 16:41:26 +0000

    default pattern name to the filename

--- tests/tests/test_views.py
+++ tests/tests/test_views.py
@@ -30,3 +30,24 @@ class ViewsTestCase(SimpleTestCase):
 
         self.assertEqual(display_link.text.strip(), "Pretty name for test molecule")
         self.assertEqual(render_link.text.strip(), "Pretty name for test molecule")
+
+    def test_pretty_names_from_filename(self):
+        test_molecule_display_url = reverse(
+            'pattern_library:display_pattern',
+            kwargs={'pattern_template_name': 'patterns/molecules/test_molecule/test_molecule_no_context.html'},
+        )
+        test_molecule_render_url = reverse(
+            'pattern_library:render_pattern',
+            kwargs={'pattern_template_name': 'patterns/molecules/test_molecule/test_molecule_no_context.html'},
+        )
+
+        response = self.client.get(test_molecule_display_url)
+        self.assertEqual(response.status_code, 200)
+
+        soup = BeautifulSoup(response.content)
+
+        display_link = soup.select_one(f'a[href="{test_molecule_display_url}"]')
+        render_link = soup.select_one(f'a[href="{test_molecule_render_url}"]')
+
+        self.assertEqual(display_link.text.strip(), "test_molecule_no_context.html")
+        self.assertEqual(render_link.text.strip(), "test_molecule_no_context.html")
