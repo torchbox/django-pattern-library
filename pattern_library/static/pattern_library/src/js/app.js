3c267cc07ca72bf871514537e2d1ddbba75ed5dc
Simon Evans <simon.evans@torchbox.com>
2019-02-02 12:12:51 +0000

    Add tabbed listing

--- pattern_library/static/pattern_library/src/js/app.js
+++ pattern_library/static/pattern_library/src/js/app.js
@@ -76,11 +76,44 @@ import hljs from 'highlight.js/lib/highlight';
         });
     }
 
+    function tabbedContent() {
+        let i;
+        let tabItem = document.querySelectorAll('.tabbed-content__heading');
+
+        function tabs(tabClickEvent) {
+            for (let i = 0; i < tabItem.length; i++) {
+                tabItem[i].classList.remove('tabbed-content__heading--active');
+            }
+
+            let clickedTab = tabClickEvent.currentTarget;
+
+            clickedTab.classList.add('tabbed-content__heading--active');
+            tabClickEvent.preventDefault();
+
+            let contentPanes = document.querySelectorAll('.tabbed-content__item');
+
+            for (i = 0; i < contentPanes.length; i++) {
+                contentPanes[i].classList.remove('tabbed-content__item--active');
+            }
+
+            let anchorReference = tabClickEvent.target;
+            let activePaneId = anchorReference.getAttribute('href');
+            let activePane = document.querySelector(activePaneId);
+
+            activePane.classList.add('tabbed-content__item--active');
+        }
+
+        for (i = 0; i < tabItem.length; i++) {
+            tabItem[i].addEventListener('click', tabs)
+        }
+    }
+
     document.addEventListener('DOMContentLoaded', () => {
         addSyntaxHighlighting();
         toggleNavItems();
         resizeIframe();
         setIframeSize();
         toggleNav();
+        tabbedContent();
     });
 }
