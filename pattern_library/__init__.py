8909cf5d15b739c53c1bb4b38448516e1764b5c1
Chris Lawton <chris.lawton@torchbox.com>
2019-03-19 14:46:32 +0000

    Bump to 0.2.1

--- pattern_library/__init__.py
+++ pattern_library/__init__.py
@@ -2,7 +2,7 @@ import importlib
 from functools import wraps
 
 
-__version__ = '0.2.0'
+__version__ = '0.2.1'
 
 default_app_config = 'pattern_library.apps.PatternLibraryAppConfig'
 settings = None
