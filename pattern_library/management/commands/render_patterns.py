cb1ce4cf0c95aae98e1916a4d473723b295c20ae
Thibaud Colas <thibaudcolas@gmail.com>
2020-09-24 14:55:29 +0100

    Refine implementation for real-world usage

--- pattern_library/management/commands/render_patterns.py
+++ pattern_library/management/commands/render_patterns.py
@@ -9,28 +9,61 @@ from pattern_library.utils import (
 
 
 class Command(BaseCommand):
-    help = "Renders all patterns."
+    help = "Renders all django-pattern-library patterns to HTML files, in a directory structure."
+
+    def add_arguments(self, parser):
+        super().add_arguments(parser)
+        parser.add_argument(
+            '--output',
+            '-o',
+            action='store',
+            dest='output_dir',
+            default='dpl-rendered-patterns',
+            help='Directory where to render your patterns',
+            type=str,
+        )
+        parser.add_argument(
+            '--dry-run',
+            action='store_true',
+            help="Render the patterns without writing them to disk.",
+        )
 
     def handle(self, **options):
+        self.verbosity = options['verbosity']
+        self.dry_run = options['dry_run']
+        self.output_dir = options['output_dir']
+
         templates = get_pattern_templates()
+
         factory = RequestFactory()
         request = factory.get('/')
-        parent_dir = Path.cwd().joinpath('dpl-static')
+
+        if self.verbosity >= 2:
+            self.stdout.write(f'Target directory: {self.output_dir}')
+
+        # Resolve the output dir according to the directory the command is run from.
+        parent_dir = Path.cwd().joinpath(self.output_dir)
         parent_dir.mkdir(exist_ok=True)
+
         self.render_group(request, parent_dir, templates)
 
     def render_group(self, request, parent_dir: Path, pattern_templates):
         for template in pattern_templates['templates_stored']:
-            print(template.origin.template_name)
-            print('-------------------')
+            if self.verbosity >= 2:
+                self.stdout.write(f'Pattern: {template.pattern_name}')
+            if self.verbosity >= 1:
+                self.stdout.write(template.origin.template_name)
+
             render_path = parent_dir.joinpath(template.pattern_name)
-            render_path.write_text(render_pattern(request, template.origin.template_name))
+            rendered_pattern = render_pattern(request, template.origin.template_name)
+            render_path.write_text(rendered_pattern)
 
         if not pattern_templates['template_groups']:
             return
 
         for pattern_type_group, pattern_templates in pattern_templates['template_groups'].items():
-            print(pattern_type_group)
+            if self.verbosity >= 2:
+                self.stdout.write(f'Group: {pattern_type_group}')
             group_parent = parent_dir.joinpath(pattern_type_group)
             group_parent.mkdir(exist_ok=True)
             self.render_group(request, group_parent, pattern_templates)
