984aa52834fa4f6c321bcceae8c144867b71998b
Thibaud Colas <thibaudcolas@gmail.com>
2021-05-19 14:21:40 +0100

    Update pattern library urlpatterns for compatibility with Django 4.0

--- tests/urls.py
+++ tests/urls.py
@@ -1,13 +1,13 @@
 from django.conf import settings
-from django.conf.urls import include, url
+from django.urls import include, path
 
 from pattern_library import urls as pattern_library_urls
 
 if settings.GITHUB_PAGES_EXPORT:
     urlpatterns = [
-        url(r'^django-pattern-library/demo/pattern-library/', include(pattern_library_urls)),
+        path('django-pattern-library/demo/pattern-library/', include(pattern_library_urls)),
     ]
 else:
     urlpatterns = [
-        url(r'^pattern-library/', include(pattern_library_urls)),
+        path('pattern-library/', include(pattern_library_urls)),
     ]
