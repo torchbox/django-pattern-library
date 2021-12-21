a568fd614872e775ab2137cbae0f3cc9e740e909
Thibaud Colas <thibaudcolas@gmail.com>
2021-12-21 19:48:22 +0000

    Try disabling Poetry experimental installer

--- tox_install.sh
+++ tox_install.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 set -e
 
+# For compatibility with Python 3.10.
+# See https://github.com/python-poetry/poetry/issues/4210.
+poetry config experimental.new-installer false
 poetry install
 
 if [ ! -z "$@" ]
