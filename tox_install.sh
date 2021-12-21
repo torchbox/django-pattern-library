499fdb52afa90206ba5c9e678f54faee6975d15b
Thibaud Colas <thibaudcolas@gmail.com>
2021-12-21 23:00:16 +0000

    Retry with latest Poetry

--- tox_install.sh
+++ tox_install.sh
@@ -1,9 +1,6 @@
 #!/bin/sh
 set -e
 
-# For compatibility with Python 3.10.
-# See https://github.com/python-poetry/poetry/issues/4210.
-poetry config experimental.new-installer false
 poetry install
 
 if [ ! -z "$@" ]
