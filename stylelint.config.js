40e8bc9648db291a31494f3dac07562fd44f74b5
Thibaud Colas <thibaudcolas@gmail.com>
2025-12-02 17:11:33 +0000

    Set up Stylelint for CSS linting

---
+++ stylelint.config.js
@@ -0,0 +1,5 @@
+/** @type {import('stylelint').Config} */
+export default {
+    // See https://github.com/torchbox/stylelint-config-torchbox for rules.
+    extends: 'stylelint-config-torchbox',
+};
